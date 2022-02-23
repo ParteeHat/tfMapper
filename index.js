9a3bdcaa0ece02732bcf8cc6a232c414885c22e4
ParteeHat <90676461+ParteeHat@users.noreply.github.com>
2022-02-23 22:53:50 +0000

    added drawing, teams, and delete button (no texture yet)

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+var currentTeam = "blu"
+var scale = 0.075
 var objectSelected = false
 var itemCreated = false
 var itemCount = 1
-var movedObject
+var movedObject = new fabric.Rect({})
 var loading = true
+var drawing = false
 var map = new Image()
 map.src = "process.png"
 let canvas = new fabric.Canvas("canvas", { backgroundImage: map.src });
 canvas.selection = false;
 canvas.backgroundColor = 'rgba(0,0,0,1)';
+canvas.hoverCursor = 'pointer'
 
 // this will run on load once
 canvas.on('after:render', function (opt) {
@@ -45,7 +49,7 @@ canvas.on('mouse:wheel', function (opt) {
 // pan
 canvas.on('mouse:down', function (opt) {
   var evt = opt.e;
-  if (objectSelected === false) {
+  if (objectSelected == false) {
     this.isDragging = true;
 
     this.lastPosX = evt.clientX;
@@ -89,9 +93,12 @@ canvas.add(rect);
 
 // create an object and remove it out of sight
 function spawn(cl) {
-  fabric.Image.fromURL('https://parteehat.github.io/LoadN/droo-lt46hn.jpg', function (oImg) {
-    canvas.add(oImg);
+  fabric.Image.fromURL('icons/' + cl + currentTeam + '.png', function (oImg) {
+    movedObject.set('opacity', 1) // keep here, will reset others when more are selected
     movedObject = oImg
+    canvas.add(movedObject);
+    movedObject.scale(scale)
+    movedObject.set('opacity', 1)
     movedObject.left = -9999999999;
     movedObject.top = -9999999999;
     itemCreated = true;
@@ -102,8 +109,8 @@ function spawn(cl) {
 canvas.on('mouse:move', function (opt) {
   if (itemCreated) {
     movedObject.set('opacity', 0.5)
-    movedObject.left = (opt.absolutePointer.x - movedObject.width / 2);
-    movedObject.top = (opt.absolutePointer.y - movedObject.height / 2);
+    movedObject.left = (opt.absolutePointer.x - (scale * (movedObject.width / 2)));
+    movedObject.top = (opt.absolutePointer.y - (scale *(movedObject.height / 2)));
     movedObject.setCoords();
     canvas.renderAll();
   }
@@ -118,14 +125,59 @@ canvas.on('mouse:down', function (opt) {
 });
 
 // create listeners for each button
+document.getElementById('switch').addEventListener('click', function() {
+  if (document.getElementById('switch').checked) {
+    currentTeam = "red"
+  } else {
+    currentTeam = "blu"
+  }
+});
 document.getElementById("pscout").addEventListener("click", function () { spawn("pscout") });
 document.getElementById("fscout").addEventListener("click", function () { spawn("fscout") });
 document.getElementById("psoldier").addEventListener("click", function () { spawn("psoldier") });
 document.getElementById("rsoldier").addEventListener("click", function () { spawn("rsoldier") });
 document.getElementById("pyro").addEventListener("click", function () { spawn("pyro") });
-document.getElementById("demo").addEventListener("click", function () { spawn("demo") });
+document.getElementById("demoman").addEventListener("click", function () { spawn("demoman") });
 document.getElementById("heavy").addEventListener("click", function () { spawn("heavy") });
 document.getElementById("engineer").addEventListener("click", function () { spawn("engineer") });
 document.getElementById("medic").addEventListener("click", function () { spawn("medic") });
 document.getElementById("sniper").addEventListener("click", function () { spawn("sniper") });
 document.getElementById("spy").addEventListener("click", function () { spawn("spy") });
+document.getElementById("delete").addEventListener("click", function () {
+  canvas.remove(canvas.getActiveObject());
+});
+
+//Drawing
+var drawingModeEl = document.getElementById('drawing-mode'),
+drawingOptionsEl = document.getElementById('drawing-mode-options'),
+drawingColorEl = document.getElementById('drawing-color'),
+drawingLineWidthEl = document.getElementById('drawing-line-width')
+//Toggle Drawing
+drawingModeEl.onclick = function() {
+  canvas.discardActiveObject();
+  canvas.isDrawingMode = !canvas.isDrawingMode;
+  if (canvas.isDrawingMode) {
+    objectSelected = true
+    drawing = true
+    drawingModeEl.innerHTML = 'Cancel drawing mode';
+    drawingOptionsEl.style.display = '';
+  }
+  else {
+    objectSelected = false
+    drawing = false
+    drawingModeEl.innerHTML = 'Enter drawing mode';
+    drawingOptionsEl.style.display = 'none';
+  }
+};
+
+drawingColorEl.onchange = function() {
+  canvas.freeDrawingBrush.color = drawingColorEl.value;
+};
+drawingLineWidthEl.onchange = function() {
+  canvas.freeDrawingBrush.width = parseInt(drawingLineWidthEl.value, 10) || 1;
+};
+
+if (canvas.freeDrawingBrush) {
+  canvas.freeDrawingBrush.color = drawingColorEl.value;
+  canvas.freeDrawingBrush.width = parseInt(drawingLineWidthEl.value, 10) || 1;
+}
